6b7cbf0e1edf05bce21e46ca9454f9cb6930f42a
Dong-Han-Kim <dh32110@gmail.com>
2024-05-23 22:22:36 +0900

    수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), vanillaExtractPlugin()],
-	base: '/shop',
+	base: 'https://dong-han-kim.github.io/DHmall/',
 });
